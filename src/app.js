9b3d745e31f261f9d4107d604e9fd3d855703c19
biduzilla <rlyowlly19@gmail.com>
2023-01-25 11:12:18 -0300

    finalização do crud

--- src/app.js
+++ src/app.js
@@ -24,11 +24,36 @@ app.get('/livros', (req, res) => {
     res.status(200).json(livros)
 })
 
+
+app.get('/livros/:id', (req, res) => {
+    let index = buscarLivro(req.params.id);
+    res.json(livros[index])
+})
+
 app.post('/livros', (req, res) => {
     livros.push(req.body)
     res.status(201).send({
         mensagem: "Livro Cadastrado"
     })
 })
+
+app.put('/livros/:id', (req, res) => {
+    let index = buscarLivro(req.params.id);
+    livros[index].titulo = req.body.titulo
+    res.status(201).json(livros)
+})
+
+app.delete('/livros/:id', (req, res) => {
+    let {id} = req.params
+    let index = buscarLivro(id);
+    livros.splice(index, 1)
+    res.status(200).send({
+        mensagem: "Livro Removido com Sucesso"
+    })
+})
+
+function buscarLivro(id){
+    return livros.findIndex(livro => livro.id == id)
+}
 
 export default app
